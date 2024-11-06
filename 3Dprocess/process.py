0bc3b6051eb0f44cc61f80aa8918adc5c7a3b9ba
liucanpku <8793480+liucanpku@user.noreply.gitee.com>
2024-11-06 18:34:23 +0800

    simulate

--- 3Dprocess/process.py
+++ 3Dprocess/process.py
@@ -41,6 +41,11 @@ print("X:", x_min, x_max)
 print("Z:", z_min, z_max)
 
 
+Y_grid = 512
+X_grid = (x_max - x_min) * Y_grid / (y_max - y_min)
+Z_grid = 101
+
+
 # 每个点对应的值
 # values = np.array([10, 15, 20, 25, 30])
 
@@ -55,17 +60,28 @@ print("point number", len(points), "value number", len(values))
 # grid_x, grid_y, grid_z = np.mgrid[0:7:50j, 0:7:50j, 0:5:50j]
 
 
-grid_x, grid_y, grid_z = np.mgrid[x_min:x_max:50j, y_min:y_max:50j, z_min:z_max:50j]
+grid_x, grid_y, grid_z = np.mgrid[x_min:x_max:X_grid * 1j, y_min:y_max:Y_grid * 1j, z_min:z_max:Z_grid * 1j]
 
 # 插值到规则网格
 grid_values = griddata(points, values, (grid_x, grid_y, grid_z), method='linear')
 
 print("grid_values shape", grid_values.shape)
 
+slice_data = grid_values[:, :, 50]
+print("slice_data shape", slice_data.shape)
+
+# 设置颜色映射的取值范围
+vmin = np.min(slice_data)  # 或者你可以手动设置
+vmax = np.max(slice_data)  # 或者你可以手动设置
+
+# 保存切片为 PNG 图像
+plt.imsave('slice_50.png', slice_data, cmap='viridis')
+
+
 # 可以选择可视化某个切片来查看插值结果
-plt.imshow(grid_values[:, :, 25], extent=(x_min, x_max, y_min, y_max), origin='lower', cmap='viridis')
+plt.imshow(grid_values[:, :, 50], extent=(x_min, x_max, y_min, y_max), origin='lower', cmap='viridis')
 plt.colorbar(label='Interpolated values')
 plt.xlabel('X')
 plt.ylabel('Y')
 plt.title('Interpolated slice at Z = midpoint')
-plt.savefig('./3Dprocess/interpolation.png')
+plt.savefig('./temp_data/interpolation.png')
